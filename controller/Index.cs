fe271d18b4d3ef820514444fb4bb7e1dedc2ac4a
Stephen <stephencurryliu@163.com>
2024-01-04 14:13:52 +0800

    hello

---
+++ controller/Index.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreDemo.Controller;
+[ApiController]
+[Route("/index")]
+public class Index
+{
+
+    [HttpGet("/hello")]
+    public string Hello()
+    {
+        return "HelloWorld";
+    }
+
+}
